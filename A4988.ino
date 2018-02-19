463c448d187a97686a4b8553dc7c5562d4971b1c
koma-koma <keikei12@gmail.com>
2018-02-19 18:32:34 +0900

    first commit

---
+++ A4988.ino
@@ -0,0 +1,74 @@
+// A4988を用いたステッピングモーター制御
+
+#define ENA 6   // Enable　→　Arduinoの6番ピンへ
+#define STEP 5  // Step →　Arduinoの5番ピンへ
+#define DIR 4   // Direction →　Arduinoの4番ピンへ
+
+void setup() { 
+  pinMode(ENA,OUTPUT);
+  pinMode(STEP,OUTPUT);
+  pinMode(DIR,OUTPUT);
+  digitalWrite(ENA,LOW); // Set Enable low　→　Low状態でEnable
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  if (Serial.available()) {
+    String val = Serial.readString();
+    Serial.println("move " + val + "steps");
+    move_step(0, val.toInt());
+  }
+//   digitalWrite(DIR,HIGH); // Set Dir High　→　回転方向をセット
+//  for (int i = 0; i < 200; i++) {
+//    digitalWrite(STEP,HIGH); // 5番ピンをHigh状態へ
+//    delayMicroseconds(500); // ５００ｍｓ待って
+//    digitalWrite(STEP,LOW); // 5番ピンをLow状態へ
+//    delayMicroseconds(1000); // ５００ｍｓ待って 
+//  }
+  /*
+  delay(1000); // １秒待つ
+
+  digitalWrite(DIR,LOW); // Set Dir Low　→　回転方向を反対方向へセット
+  for (int i = 0; i < 200; i++) {
+    digitalWrite(STEP,HIGH); // 5番ピンをHigh状態へ
+    delayMicroseconds(500); // ５００ｍｓ待って
+    digitalWrite(STEP,LOW); // 5番ピンをLow状態へ
+    delayMicroseconds(1000); // ５００ｍｓ待って 
+  }
+  delay(1000); // １秒待つ
+  */
+}
+
+void rotate(bool dir, int spd) {
+  int deltime = spd;
+  
+  if (dir) {
+    digitalWrite(DIR,HIGH); // Set Dir High　→　回転方向をセット
+  } else {
+    digitalWrite(DIR,LOW); // Set Dir High　→　回転方向をセット
+  }
+  for (int i = 0; i < 200; i++) {
+    digitalWrite(STEP,HIGH); // 5番ピンをHigh状態へ
+    delayMicroseconds(500); // ５００ｍｓ待って
+    digitalWrite(STEP,LOW); // 5番ピンをLow状態へ
+    delayMicroseconds(deltime); // ５００ｍｓ待って 
+  }
+}
+
+void move_step(bool dir, int steps) {
+  if (dir) {
+    digitalWrite(DIR,HIGH); // Set Dir High　→　回転方向をセット
+  } else {
+    digitalWrite(DIR,LOW); // Set Dir High　→　回転方向をセット
+  }
+  
+  for (int i = 0; i < steps; i++) {
+    digitalWrite(STEP,HIGH); // 5番ピンをHigh状態へ
+    delayMicroseconds(500); // ５００ｍｓ待って
+    digitalWrite(STEP,LOW); // 5番ピンをLow状態へ
+    delayMicroseconds(1000); // ５００ｍｓ待って 
+  }
+}
+
